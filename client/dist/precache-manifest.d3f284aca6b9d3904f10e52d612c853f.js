b8e40240bfe32cb68cc53609f235bb43ea94a8fb
Christopher Brown <chbphone55@gmail.com>
2018-09-19 15:30:39 -0600

    pppppush

---
+++ client/dist/precache-manifest.d3f284aca6b9d3904f10e52d612c853f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "89e0bf48a5976b85f9aa",
+    "url": "/js/chunk-vendors.6cf3c020.js"
+  },
+  {
+    "revision": "936e5ee37f221a3592a2",
+    "url": "/js/app.e21266f8.js"
+  },
+  {
+    "revision": "b04d201cb8406d8d0942cf0bf6facfe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89e0bf48a5976b85f9aa",
+    "url": "/css/chunk-vendors.2707675e.css"
+  },
+  {
+    "revision": "936e5ee37f221a3592a2",
+    "url": "/css/app.8b3cb36b.css"
+  }
+];
